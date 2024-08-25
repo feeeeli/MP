f4b6b4f1da3e6a94991230179fae54d58f0173b2
feeeeli <feeeli2011@gmail.com>
2024-08-25 19:13:57 +0200

    Anpassungen

--- js/lightgallery.js
+++ js/lightgallery.js
@@ -24,8 +24,9 @@ const inlineGallery = lightGallery(lgContainer, {
             src: 'images/Foto_98.jpg',
             thumb: 'images/Foto_98.jpg',
             subHtml: `<div class="lightGallery-captions" >          
-                     <h4>Kerzenständer aus Kirschholz</h4>
-                    </div>`,
+                     <h4 class="tooltip">Kerzenständer aus Kirschholz</h4>
+                     <span class="tooltip-text">Kerzenständer aus Kirschholz Tooltip</span>
+                    </div> `,
         },
         {
             src: 'images/Foto_97.jpg',
@@ -38,7 +39,7 @@ const inlineGallery = lightGallery(lgContainer, {
             src: 'images/Foto_99.jpg',
             thumb: 'images/Foto_99.jpg',
             subHtml: `<div class="lightGallery-captions" >          
-                     <h4>Vase aus Robinie</h4>
+                     <h4>Vase aus Linde</h4>
                     </div>`,
         },
         {
@@ -51,21 +52,21 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_1.jpg',
             thumb: 'images/Foto_1.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Vase aus Robinie</h4>
                     </div>`,
         },
         {
-            src: 'images/Foto_2.jpg',
-            thumb: 'images/Foto_2.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            src: 'images/Foto_15.jpg',
+            thumb: 'images/Foto_15.jpg',
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Multigadget</h4>
                     </div>`,
         },
         {
             src: 'images/Foto_3.jpg',
             thumb: 'images/Foto_3.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Flaschenverschlüsse</h4>
                     </div>`,
         },
@@ -73,7 +74,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_4.jpg',
             thumb: 'images/Foto_4.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Eibenblume mit Herz</h4>
                     </div>`,
         },
@@ -81,7 +82,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_5.jpg',
             thumb: 'images/Foto_5.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Flaschenlicht aus Walnussholz</h4>
                     </div>`,
         },
@@ -89,7 +90,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_6.jpg',
             thumb: 'images/Foto_6.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Ostereidose aus Walnussholz</h4>
                     </div>`,
         },
@@ -97,7 +98,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_7.jpg',
             thumb: 'images/Foto_7.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Pflaume mit gelochiertem Deckel</h4>
                     </div>`,
         },
@@ -105,7 +106,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_88.jpg',
             thumb: 'images/Foto_88.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Skulptur aus Walnussholz</h4>
                     </div>`,
         },
@@ -113,7 +114,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_9.jpg',
             thumb: 'images/Foto_9.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Apfel-Lufterfrischer</h4>
                     </div>`,
         },
@@ -121,7 +122,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_10.jpg',
             thumb: 'images/Foto_10.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Apfel-Lufterfrischer</h4>
                     </div>`,
         },
@@ -129,7 +130,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_11.jpg',
             thumb: 'images/Foto_1.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Vase aus Robinie</h4>
                     </div>`,
         },
@@ -137,7 +138,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_12.jpg',
             thumb: 'images/Foto_12.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Schüssel und Vase aus Walnussholz</h4>
                     </div>`,
         },
@@ -145,7 +146,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_13.jpg',
             thumb: 'images/Foto_13.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Vase aus Apfelbaum</h4>
                     </div>`,
         },
@@ -153,43 +154,37 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_140.jpg',
             thumb: 'images/Foto_140.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Dose aus Walnussholz</h4>
                     </div>`,
         },
 
-        {
-            src: 'images/Foto_15.jpg',
-            thumb: 'images/Foto_15.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
-                     <h4>Multigadget</h4>
-                    </div>`,
-        },
+
         {
             src: 'images/Foto_17.jpg',
             thumb: 'images/Foto_17.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Walnussschüssel Wiking Bowl</h4>
                     </div>`,
         },
         {
             src: 'images/Foto_18.jpg',
             thumb: 'images/Foto_18.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Pflaumendose strukturiert</h4>
                     </div>`,
         },
         {
             src: 'images/Foto_19.jpg',
             thumb: 'images/Foto_19.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Walnussschüssel strukturiert</h4>
                     </div>`,
         },
         {
             src: 'images/Foto_20.jpg',
             thumb: 'images/Foto_20.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Kuksa Tasse Walnuss</h4>
                     </div>`,
         },
@@ -197,7 +192,7 @@ const inlineGallery = lightGallery(lgContainer, {
         {
             src: 'images/Foto_21.jpg',
             thumb: 'images/Foto_21.jpg',
-            subHtml: `<div class="lightGallery-captions" style="color: black;" >          
+            subHtml: `<div class="lightGallery-captions" >          
                      <h4>Dübelspiel aus Apfelbaum</h4>
                     </div>`,
         },
